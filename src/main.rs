7a1d56d6fe96e6135567ead47ce391761de49eaa
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2019-04-22 16:12:09 +0200

    What a jorney!

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
+use serde_json::{Result, Value, json};
+use std::string::String;
 
-use serde::Deserialize;
-
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum Command {
     FindImage {
         path: String,
     },
     ResizeImage {
         source: String,
-        sizes: Vec<(String, u32, u32)>
-    }
+        sizes: Vec<(String, u32, u32)>,
+    },
+    WrongCommand,
 }
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum Event {
     ImageFound {
         path: String
@@ -28,14 +26,30 @@ pub enum Event {
     },
     ImageResizeFailed {
         target: String,
-        reason: String
-    }
+        reason: String,
+    },
 }
 
 pub fn create_from_json_string(json: String) -> Command {
-    Command::FindImage {
-        path: "no-op".to_string()
+    let json_structure: Value = serde_json::from_str(&json).unwrap();
+
+    let command_type = &json_structure["type"];
+
+    if command_type == "find" {
+        return match &json_structure["path"] {
+            Value::String(String) => Command::FindImage {
+                path: String::from(json_structure["path"].as_str().unwrap())
+            },
+            _ => Command::WrongCommand
+        };
+    } else if command_type == "resize" {
+        return Command::ResizeImage {
+            source: String::from(json_structure["source"].as_str().unwrap()),
+            sizes: vec![]
+        }
     }
+
+    Command::WrongCommand
 }
 
 #[cfg(test)]
@@ -49,8 +63,53 @@ mod tests {
                 path: "/path/to/file".to_string()
             },
             create_from_json_string(
-                "{\"type\":\"find\",\"path\":\"/path/to/file\"}".to_string()
+                String::from("{\"type\":\"find\",\"path\":\"/path/to/file\"}")
             )
         )
+    }
+
+    #[test]
+    fn when_type_is_wrong_creates_wrong_command() {
+        assert_eq!(
+            Command::WrongCommand,
+            create_from_json_string(
+                String::from("{\"type\":\"something\"}")
+            )
+        )
+    }
+
+    #[test]
+    fn when_type_is_not_provided_creates_wrong_command() {
+        assert_eq!(Command::WrongCommand, create_from_json_string(
+            String::from("{}")
+        ));
+    }
+
+    #[test]
+    fn when_path_is_missing_for_find_command_creates_wrong_command() {
+        assert_eq!(Command::WrongCommand, create_from_json_string(
+            String::from("{\"type\": \"find\"}")
+        ));
+    }
+
+    #[test]
+    fn when_resize_image_type_is_provided_creates_resize_image_command() {
+        assert_eq!(
+            Command::ResizeImage {
+                source: String::from("path/to/image.jpg"),
+                sizes: vec![
+                    (String::from("path/to/320x400/image.jpg"), 320, 400)
+                ],
+            },
+            create_from_json_string(
+                String::from(
+                    "{\
+                        \"type\":\"resize\", \
+                        \"source\":\"path/to/image.jpg\", \
+                        \"sizes\": [[\"path/to/320x400/image.jpg\", 320, 400]]\
+                    }"
+                )
+            )
+        );
     }
 }
