d341b76def92af5fa7766a2f507b6e81c6378ed4
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2019-07-04 10:15:41 +0200

    Ditch old command trait implementation in favor of enums + trait public API

--- src/new_commands.rs
+++ src/new_commands.rs
@@ -1,247 +1,152 @@
 use futures::Future;
 use std::fmt::Debug;
-use serde_json::Value;
+use serde_json::{json, Value, to_string as json_to_string};
 
-trait Command: Debug {
-    fn merge_with(self, other: Self) -> Box<Self>;
-
-    fn hash_key(&self) -> String;
-
-   // fn process_handler(&self)
-   //                    -> Box<Future<Item = Event, Error = std::io::Error>>;
+pub trait Command {
+    fn process_handler(&self) -> Box<Future<Item=Event, Error=std::io::Error>>;
 }
 
-trait Event {
-    fn to_string(&self) -> String;
+pub trait Event {
+    fn as_json(&self) -> String;
 }
 
-#[derive(Debug, PartialEq)]
-pub struct WrongCommand {
-    reason: String
-}
+type ImagePath = String;
+type WrongInputDescription = String;
+type ImageResizeFailureReason = String;
+type Width = u32;
+type Height = u32;
 
-#[derive(Debug, PartialEq)]
-pub struct FindImageCommand {
-    path: String
-}
+struct ImageSize(Width, Height);
 
-#[derive(Debug, PartialEq)]
-pub struct ImageTarget(String, u32, u32);
-
-#[derive(Debug, PartialEq)]
-pub struct ResizeImageCommand {
-    path: String,
-    targets: Vec<ImageTarget>
-}
+struct ImageTarget(ImagePath, ImageSize);
 
 enum Commands {
-    WrongCommand {
-        reason: String
+    InvalidInput(WrongInputDescription),
+    FindImage(ImagePath),
+    ResizeImage(ImagePath, Vec<ImageTarget>),
+}
+
+enum Events {
+    InvalidInputProvided(WrongInputDescription),
+    ImageFound(ImagePath),
+    ImageNotFound(ImagePath),
+    ImageResizeCompleted(ImageTarget),
+    ImageResizeFailed(ImageTarget, ImageResizeFailureReason),
+}
+
+impl Command for Commands
+{
+    fn process_handler(&self) -> Box<Future<Item=Event, Error=std::io::Error>> {
+        unimplemented!()
     }
 }
 
-impl Commands::WrongCommand {
-    fn new(reason: String) -> Self {
-        Commands::WrongCommand { reason }
-    }
-}
-
-impl Command for WrongCommand {
-    fn merge_with(self, _: WrongCommand) -> WrongCommand {
-        self
-    }
-
-    fn hash_key(&self) -> String {
-        format!("WrongCommand::{}", self.reason)
-    }
-}
-
-impl FindImageCommand {
-    fn new(path: String) -> Self {
-        FindImageCommand { path }
-    }
-}
-
-impl Command for FindImageCommand {
-    fn merge_with(self, other: FindImageCommand) -> FindImageCommand {
-        self
-    }
-
-    fn hash_key(&self) -> String {
-        format!("FindImageCommand::{}", self.path)
-    }
-}
-
-impl ResizeImageCommand {
-    fn new(path: String, targets: Vec<ImageTarget>) -> Self {
-        ResizeImageCommand { path, targets }
-    }
-}
-
-impl Command for ResizeImageCommand {
-    fn merge_with(self, other: Self) -> Self {
-        ResizeImageCommand {
-            targets: self.targets
-                .into_iter()
-                .chain(other.targets.into_iter())
-                .collect(),
-            ..self
+impl Event for Events {
+    fn as_json(&self) -> String {
+        match self {
+            Events::InvalidInputProvided(reason) => json_event_invalid_input(reason),
+            Events::ImageFound(path) => json_event_image_found(path),
+            Events::ImageNotFound(path) => json_event_image_not_found(path),
+            Events::ImageResizeCompleted(target)
+                => json_event_image_resize_completed(target),
+            Events::ImageResizeFailed(target, reason)
+                => json_event_image_resize_failed(target, reason),
         }
     }
-
-    fn hash_key(&self) -> String {
-        format!("ResizeImageCommand::{}", self.path)
-    }
 }
 
-pub fn create_command_from_json_string(json: String) -> impl Command
-{
-    match serde_json::from_str(&json) {
-        Ok(value) => create_command_from_parsed_json_value(value),
-        _ => create_wrong_command("")
-    }
+fn json_event_invalid_input(reason: &WrongInputDescription) -> String {
+    json_to_string(&json!({
+        "event": "invalid_input",
+        "reason": reason
+    })).unwrap()
 }
 
-fn create_command_from_parsed_json_value(json: Value) -> impl Command {
-    match json["command"].as_str() {
-        Some("find") => match &json["path"] {
-            Value::String(path) => create_find_image_command(path.to_string()),
-            _ => create_wrong_command("")
-        }
-        _ =>  create_wrong_command("")
-    }
+fn json_event_image_found(path: &ImagePath) -> String {
+    json_to_string(&json!({
+        "event": "image_found",
+        "path": path
+    })).unwrap()
 }
 
-fn create_find_image_command(path: String) -> impl Command {
-    FindImageCommand::new(path)
+fn json_event_image_not_found(path: &ImagePath) -> String {
+    json_to_string(&json!({
+        "event": "image_not_found",
+        "path": path
+    })).unwrap()
 }
 
-fn create_wrong_command(reason: &str) -> impl Command {
-    WrongCommand::new(reason.into())
+fn json_event_image_resize_completed(target: &ImageTarget) -> String {
+    let ImageTarget(path, ImageSize(width, height)) = target;
+    json_to_string(&json!({
+        "event": "image_resize_completed",
+        "path": path,
+        "size": [width, height]
+    })).unwrap()
+}
+
+fn json_event_image_resize_failed(target: &ImageTarget, reason: &ImageResizeFailureReason) -> String {
+    let ImageTarget(path, ImageSize(width, height)) = target;
+    json_to_string(&json!({
+        "event": "image_resize_failed",
+        "path": path,
+        "size": [width, height],
+        "reason": reason
+    })).unwrap()
 }
 
 #[cfg(test)]
-mod wrong_command {
-
+mod event_serialization {
     use super::*;
 
     #[test]
-    fn does_not_merge_with_other_wrong_commands() {
-        let command = WrongCommand::new("Reasons".into());
-
+    fn serializes_invalid_input_event_as_json() {
         assert_eq!(
-            WrongCommand::new("Reasons".into()),
-            command.merge_with(WrongCommand::new("Something Else".into()))
+            r#"{"event":"invalid_input","reason":"Can't parse input line as json"}"#,
+            Events::InvalidInputProvided("Can't parse input line as json".into()).as_json()
         );
     }
 
     #[test]
-    fn returns_reason_as_a_hash_key () {
-       assert_eq!(
-           "WrongCommand::Some Reason Value",
-           WrongCommand::new("Some Reason Value".into()).hash_key()
-       );
-    }
-}
-
-#[cfg(test)]
-mod find_image
-{
-    use super::*;
-
-    #[test]
-    fn returns_same_command_after_merging_command_with_itself () {
-        let command = FindImageCommand::new("/some/file/path.jpg".into());
-
+    fn serializes_image_found_event_as_json() {
         assert_eq!(
-            FindImageCommand::new("/some/file/path.jpg".into()),
-            command.merge_with(FindImageCommand::new("/some/file/path2.jpg".into()))
-        );
-    }
-
-    #[test]
-    fn returns_file_path_as_a_hash_key () {
-        let command = FindImageCommand::new("/very/nice/file/path.jpg".into());
-
-        assert_eq!(
-            "FindImageCommand::/very/nice/file/path.jpg",
-            command.hash_key()
-        );
-    }
-}
-
-#[cfg(test)]
-mod image_resize
-{
-    use crate::new_commands::{ResizeImageCommand, ImageTarget, Command};
-
-    #[test]
-    fn merges_targets_from_another_command () {
-        let command = ResizeImageCommand::new(
-            "/some/path.jpg".into(),
-            vec![ImageTarget("/some/path-100x200.jpg".into(), 100, 200)]
-        );
-
-       assert_eq!(
-           ResizeImageCommand::new(
-               "/some/path.jpg".into(),
-               vec![
-                   ImageTarget("/some/path-100x200.jpg".into(), 100, 200),
-                   ImageTarget("/some/path-200x300.jpg".into(), 200, 300),
-               ]
-           ),
-           command.merge_with(
-               ResizeImageCommand::new(
-                   "/some/path.jpg".into(),
-                   vec![ImageTarget("/some/path-200x300.jpg".into(), 200, 300)]
-               )
-           )
-       );
-    }
-
-    #[test]
-    fn returns_source_image_path_as_hash_key () {
-       assert_eq!(
-           "ResizeImageCommand::/image/path.jpg",
-           ResizeImageCommand::new("/image/path.jpg".into(), vec![]).hash_key()
-       );
-    }
-}
-
-#[cfg(test)]
-mod factory {
-    use super::*;
-
-    fn create_command (json: &str) -> String {
-        format!("{:?}", create_command_from_json_string(json.into()))
-    }
-
-    #[test]
-    fn given_invalid_json_string_creates_wrong_command_with_info_on_wrong_json_structure() {
-       assert_eq!(
-          r#"WrongCommand { reason: "Invalid JSON structure is provided" }"#,
-          create_command("{,}")
-       );
-    }
-
-    #[test]
-    fn creates_find_image_command_from_json() {
-        assert_eq!(
-            r#"FindImageCommand { path: "/path/to/file" )"#,
-            create_command(
-                "{\"command\":\"find\",\"path\":\"/path/to/file\"}".into()
-            )
+            r#"{"event":"image_found","path":"/path/to/image.jpg"}"#,
+            Events::ImageFound("/path/to/image.jpg".into()).as_json()
         )
     }
 
     #[test]
-    fn when_type_is_wrong_creates_wrong_command() {
+    fn serializes_image_not_found_event_as_json() {
         assert_eq!(
-            r#"WrongCommand { reason: "Unkown command type provided" }"#,
-            create_command(
-                "{\"command\":\"something\"}"
-            )
+            r#"{"event":"image_not_found","path":"/path/to/image.jpg"}"#,
+            Events::ImageNotFound("/path/to/image.jpg".into()).as_json()
         )
     }
 
+    #[test]
+    fn serializes_image_resize_completed_as_json() {
+        assert_eq!(
+            r#"{"event":"image_resize_completed","path":"/path/to/image1.jpg","size":[300,200]}"#,
+            Events::ImageResizeCompleted(
+                ImageTarget(
+                    "/path/to/image1.jpg".into(),
+                    ImageSize(300, 200),
+                )
+            ).as_json()
+        );
+    }
+
+    #[test]
+    fn serializes_image_resize_failed_as_json() {
+        assert_eq!(
+            r#"{"event":"image_resize_failed","path":"/path/to/image1.jpg","reason":"Failed!","size":[300,200]}"#,
+            Events::ImageResizeFailed(
+                ImageTarget(
+                    "/path/to/image1.jpg".into(),
+                    ImageSize(300, 200),
+                ),
+                "Failed!".into()
+            ).as_json()
+        );
+    }
 }
