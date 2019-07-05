8b9d22cd693f360eab02c8bd172832856713accd
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2019-07-05 15:37:05 +0200

    Create first combinator for input data stream

--- src/json_stream.rs
+++ src/json_stream.rs
@@ -1,41 +1,93 @@
 use tokio::prelude::*;
 use futures::prelude::*;
-use serde_json::Value as JsonValue;
-use std::io;
-use tokio::codec::Framed;
-use crate::json_codec::JsonCodec;
-use serde::de::Error;
-use tokio::sync::mpsc::Receiver;
+use tokio::sync::mpsc::UnboundedReceiver;
+use futures::stream::{Stream, Chain};
+use std::fmt::Error;
+use futures::OrElse;
 
-enum Connection {
-    InputError(io::Error),
-    InputCommand(JsonValue),
-    OutputEvent(JsonValue)
+#[derive(Debug, PartialEq)]
+enum Connection<T: Sized,E: Sized>  {
+    InputError(E),
+    InputCommand(T)
 }
 
-fn create_stream<S: Stream + Sized, U: Sized>(
-    read_stream: S<Item=JsonValue, Error=io::Error>,
-    event_stream: S<Item=JsonValue, Error=()>) -> S<Item=Connection, Error=()>
+#[derive(Debug, PartialEq)]
+struct ConnectionError(());
+
+fn create_merged_input_stream<S1: Stream + Sized>(input_stream: S1)
+    -> impl Stream<Item=Connection<S1::Item, S1::Error>, Error=ConnectionError>
 {
-    stream::iter(vec![])
+    stream::empty().chain(
+        input_stream.map(|v| Connection::InputCommand(v))
+            .or_else( |e| future::ok(Connection::InputError(e)) )
+    )
 }
 
 #[cfg(test)]
 mod stream_merger
 {
     use super::*;
+    use tokio::sync::mpsc::unbounded_channel;
+    use serde_json::{Value as JsonValue, json};
+    use futures::stream::Empty;
 
     #[test]
-    fn test_it_works() {
-        let stream = create_stream(
-           stream::iter(
-               vec![]
-           ),
-           stream::iter(
-               vec![]
-           )
+    fn test_it_merges_into_empty_stream() {
+        let empty_stream = stream::empty::<JsonValue, ()>();
+
+        let result = create_merged_input_stream(
+            empty_stream
         );
 
-        stream.wait().collect();
+        assert_eq!(Ok(vec![]), result.collect().wait());
+    }
+
+    #[test]
+    fn returns_commands_wrapped_into_connection_input_command() {
+        let result = create_merged_input_stream(
+            stream::iter_ok::<_, ()>(vec![
+                json!({"command": "command1"}),
+                json!({"command": "command2"})
+            ])
+        );
+
+        assert_eq!(
+            Ok(
+                vec![
+                    Connection::InputCommand(json!({"command": "command1"})),
+                    Connection::InputCommand(json!({"command": "command2"})),
+                ]
+            ),
+            result.collect().wait()
+        );
+    }
+
+    #[test]
+    fn combines_errors_with_input_command_into_one_stream_of_data () {
+        let vec: Vec<Result<JsonValue,()>> = vec![
+            Ok(json!({"command": "command1"})),
+            Err(()),
+            Err(()),
+            Ok(json!({"command": "command2"})),
+            Err(()),
+            Ok(json!({"command": "command3"})),
+        ];
+        let result = create_merged_input_stream(
+            stream::iter_result(vec)
+        );
+
+        assert_eq!(
+            Ok(
+                vec![
+                    Connection::InputCommand(json!({"command": "command1"})),
+                    Connection::InputError(()),
+                    Connection::InputError(()),
+                    Connection::InputCommand(json!({"command": "command2"})),
+                    Connection::InputError(()),
+                    Connection::InputCommand(json!({"command": "command3"})),
+                ]
+            ),
+            result.collect().wait()
+        );
     }
 }
